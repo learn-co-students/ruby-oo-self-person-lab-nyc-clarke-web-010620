6d7c7b31b5df4aca61e3e1e45f167a2984a5e79a
Mokhtar Ali <joemokhtarali@gmail.com>
2020-01-12 21:58:43 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -51,9 +51,22 @@ class Person
     return 'all about the benjamins'
   end 
   
-  def take_bath 
+  def take_bath
+    @hygiene += 4    
+    @hygiene
+    return '♪ Rub-a-dub just relaxing in the tub ♫'
+  end 
+  
+  def work_out 
+    @hygiene -= 3
+    @happiness += 2
+    return '♪ another one bites the dust ♫'
     
   end 
   
-  
+  def call_friend(friend)
+    @happiness += 3 
+    
+    
+  end 
 end 
